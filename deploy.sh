2a72701899f5cd9c75c69afd06bb9199ac64d843
JuPeg <juliana_pegoraro@hotmail.com>
2016-06-13 15:40:15 +0200

    cd to reportM2 root

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ cd mkdocs_build
 # Initialize gh-pages checkout
 DATE=`date`
 git clone https://github.com/jupeg/reportM2.git
-cd docs
+cd reportM2
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 mkdocs gh-deploy --clean -m "gh-deployed by travis $DATE"
